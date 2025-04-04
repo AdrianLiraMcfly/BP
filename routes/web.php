af6d1c44b81b62af31816daba45c957d6c491fbe
AdrianMcflyLira <jorge.liralopez11@gmail.com>
2025-04-04 00:08:15 -0600

    lol

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,6 @@ Route::middleware(['jwt'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/logout', [TwoFactorController::class, 'logout'])->name('logout');
 
 });
 
