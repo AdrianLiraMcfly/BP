f12eabdc43ac7fbf31044011dc1d09a2ca9d0d8b
AdrianMcflyLira <jorge.liralopez11@gmail.com>
2025-04-05 00:36:44 -0600

    pot vafor

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -39,14 +39,14 @@
                         </x-dropdown-link>
 
                         <!-- Authentication -->
-                        <form method="POST" action="{{ route('custom.logout') }}">
+                        <form method="POST" action="{{ route('logout') }}">
                             @csrf
-                            <x-dropdown-link :href="route('custom.logout')"
-                                onclick="event.preventDefault();
-                                         this.closest('form').submit();">
+                            <x-dropdown-link href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
+                        
                         
                     </x-slot>
                 </x-dropdown>
